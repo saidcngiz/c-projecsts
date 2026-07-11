51e00c071b8a080c2324f91bc24d26c57348ffde
said cengiz <saidcengiz.dev@gmail.com>
2026-07-11 19:39:35 +0300

    top(lama) fonksiyonu

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 
+int toplama(int x, int y) {
+    int toplam = x+y;
+    return toplam;
+}
+
 int main() {
-    int x;
+    int x = toplama(9, 15);
     printf("bir sayı girin: ");
     scanf("%d", &x);
     printf("girdiğini sayı bu muydu yoksa: %d\n", x);
 }
+
