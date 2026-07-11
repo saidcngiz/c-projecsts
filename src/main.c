da8dacae59f93e6e6a5c316d517bed6c8eb7b58e
said cengiz <saidcengiz.dev@gmail.com>
2026-07-11 20:21:43 +0300

    karakter güncelleme

--- src/main.c
+++ src/main.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
 
+char NAME = 'a';
+
+/*
 int toplama(int x, int y) {
     int toplam = x+y;
     return toplam;
 }
+*/
 
-int main() {
-    int x = toplama(9, 15);
-    printf("bir sayı girin: ");
-    scanf("%d", &x);
-    printf("girdiğini sayı bu muydu yoksa: %d\n", x);
+void isimg(char newName){
+    NAME = newName;   
 }
 
+
+int main() {
+    char temp;
+//    int x = toplama(9, 15);
+//    printf("bir sayı girin: ");
+//    scanf("%d", &x);
+//    printf("girdiğini sayı bu muydu yoksa: %d\n", x);
+    printf("şuanki harfiniz: %c \n", NAME);
+    printf("yeni harf girin: ");
+    scanf("%c", &temp);
+    isimg(temp);
+    printf("işte yeni harfiniz: %c \n", NAME);
+}
